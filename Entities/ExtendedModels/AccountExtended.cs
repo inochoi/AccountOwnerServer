92744f6ca6a8a26da146658f748c57fb126bdaab
inochoi <47161831+inochoi@users.noreply.github.com>
2019-08-20 16:23:36 -0700

    update

---
+++ Entities/ExtendedModels/AccountExtended.cs
@@ -0,0 +1,31 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.ExtendedModels
+{
+    public class AccountExtended : IEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string AccountType { get; set; }
+
+        public Guid OwnerId { get; set; }
+
+        // public IEnumerable<Account> Accounts { get; set; }
+
+        public AccountExtended()
+        {
+
+        }
+
+        public AccountExtended(Account account)
+        {
+            Id = account.Id;
+            DateCreated = account.DateCreated;
+            AccountType = account.AccountType;
+            OwnerId = account.OwnerId;
+        }
+    }
+}
--- Repository/AccountRepository.cs
+++ Repository/AccountRepository.cs
@@ -1,9 +1,13 @@
 ﻿using Contracts;
 using Entities;
+using Entities.ExtendedModels;
+using Entities.Extensions;
 using Entities.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Repository
 {
@@ -15,9 +19,45 @@ namespace Repository
 
         }
 
+        public async Task<IEnumerable<Account>> GetAllAccountsAsync()
+        {
+            return await FindAll()
+                .OrderBy(ac => ac.Id)
+                .ToListAsync();
+        }
+
+        public async Task<Account> GetAccountByIdAsync(Guid accountId)
+        {
+            return await FindByCondition(account => account.Id.Equals(accountId))
+                .DefaultIfEmpty(new Account())
+                .SingleAsync();
+        }
+        
         public IEnumerable<Account> AccountsByOwner(Guid ownerId)
         {
             return FindByCondition(a => a.OwnerId.Equals(ownerId));
+        }
+
+
+
+        public async Task CreateAccountAsync(Account account)
+        {
+            account.Id = Guid.NewGuid();
+            Create(account);
+            await SaveAsync();
+        }
+
+        public async Task DeleteAccountAsync(Account account)
+        {
+            Delete(account);
+            await SaveAsync();
+        }
+                
+        public async Task UpdateAccountAsync(Account dbAccount, Account account)
+        {
+            dbAccount.Map(account);
+            Update(dbAccount);
+            await SaveAsync();
         }
     }
 }
